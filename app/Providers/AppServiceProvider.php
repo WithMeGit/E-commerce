bc8732ad1363782366209e5b247960cc4ab0e3ce
carter-nguyen-goldenowl <carter.nguyen.goldenowl@gmail.com>
2022-05-10 10:27:29 +0700

    fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ use App\Repositories\User\UserInterface;
 use App\Repositories\User\userRepository;
 use App\Repositories\Wishlist\WishListInterface;
 use App\Repositories\Wishlist\WishlistRepository;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -44,6 +45,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
         // $this->app->singleton(CategoryInterface::class, CategoryRepository::class);
         $this->app->bind(CategoryInterface::class, CategoryRepository::class);
         $this->app->bind(BrandInterface::class, BrandRepository::class);
