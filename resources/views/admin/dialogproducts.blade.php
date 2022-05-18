6cb6456f902adba647547a9e7348aa5e9d9fcdef
carter-nguyen-goldenowl <carter.nguyen.goldenowl@gmail.com>
2022-05-18 07:50:12 +0700

    update validate price

--- resources/views/admin/dialogproducts.blade.php
+++ resources/views/admin/dialogproducts.blade.php
@@ -54,14 +54,14 @@
                 </div>
                 <div class="form-group mb-6">
                     <input type="text"
-                        onkeypress='return event.charCode >= 48 && event.charCode <= 57 && event.charCode == 46'
+                        onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode == 46'
                         class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         name="promotion_price" required value="{{ $editproduct->promotion_price }}"
                         placeholder="Promotion Price Product">
                 </div>
                 <div class="form-group mb-6">
                     <input type="text"
-                        onkeypress='return event.charCode >= 48 && event.charCode <= 57 && event.charCode == 46'
+                        onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode == 46'
                         class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         name="original_price" required value="{{ $editproduct->original_price }}"
                         placeholder="Original Price Product">
@@ -190,7 +190,7 @@
                     </div>
                     <div class="form-group mb-6">
                         <input type="text"
-                            onkeypress='return event.charCode >= 48 && event.charCode <= 57 && event.charCode == 46'
+                            onkeypress='return event.charCode >= 48 && event.charCode <= 57 || event.charCode == 46'
                             class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                             name="original_price" required placeholder="Original Price Product">
                     </div>
